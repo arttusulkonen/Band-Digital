08efaf1baac005bb38743f4110cf23f9fb27409e
arttusulkonen <arttu.sulkonen@icloud.com>
2021-12-27 12:53:46 +0300

    header/footer/custom-logo

--- functions.php
+++ functions.php
@@ -1,35 +1,49 @@
 <?php
 
+if (! function_exists('band_digital_setup')) {
+    function band_digital_setup() {
+        add_theme_support('custom-logo', [
+            'height'      => 50,
+            'width'       => 130,
+            'flex-width'  => false,
+            'flex-height' => false,
+            'header-text' => '',
+            'unlink-homepage-logo' => false, // WP 5.5
+        ]);
+    }
+    add_action('after_setup_theme', 'band_digital_setup');
+}
+
 /* Подключение стилей и скриптов */
+add_action('wp_enqueue_scripts', 'band_digital_scripts');
 
-add_action( 'wp_enqueue_scripts', 'band_digital_scripts' );
-
-function band_digital_scripts() {
-    wp_enqueue_style( 'main', get_stylesheet_uri() ); // подключаем файл css из главной папки
+function band_digital_scripts()
+{
+    wp_enqueue_style('main', get_stylesheet_uri()); // подключаем файл css из главной папки
     // bootstrap
-    wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/plugins/bootstrap/css/bootstrap.css', array('main'), null); // Подключаем этот файл строго после 'main'
+    wp_enqueue_style('bootstrap', get_template_directory_uri() . '/plugins/bootstrap/css/bootstrap.css', array('main'), null); // Подключаем этот файл строго после 'main'
     // fontawesome
-    wp_enqueue_style( 'fontawesome', get_template_directory_uri() . '/plugins/fontawesome/css/all.css', array('main'), null);
+    wp_enqueue_style('fontawesome', get_template_directory_uri() . '/plugins/fontawesome/css/all.css', array('main'), null);
     // animation
-    wp_enqueue_style( 'animate-css', get_template_directory_uri() . '/plugins/animate-css/animate.css', array('main'), null);
+    wp_enqueue_style('animate-css', get_template_directory_uri() . '/plugins/animate-css/animate.css', array('main'), null);
     // icofont
-    wp_enqueue_style( 'icofont', get_template_directory_uri() . '/plugins/icofont/icofont.css', array('main'), null);
+    wp_enqueue_style('icofont', get_template_directory_uri() . '/plugins/icofont/icofont.css', array('main'), null);
     // main css
-    wp_enqueue_style( 'band-digital', get_template_directory_uri() . '/css/style.css', array('icofont'), null); // Подключаем кастомный css файл.
+    wp_enqueue_style('band-digital', get_template_directory_uri() . '/css/style.css', array('icofont'), null); // Подключаем кастомный css файл.
 
     // переподключаем jQuery
-    wp_deregister_script( 'jquery' );
-    wp_register_script( 'jquery', get_template_directory_uri() . '/plugins/jquery/jquery.min.js');
-    wp_enqueue_script( 'jquery');
+    wp_deregister_script('jquery');
+    wp_register_script('jquery', get_template_directory_uri() . '/plugins/jquery/jquery.min.js');
+    wp_enqueue_script('jquery');
 
     // bootstrap
-    wp_enqueue_script( 'popper', get_template_directory_uri() . '/plugins/bootstrap/js/popper.min.js', array('jquery'), '1.0.0', true );
-    wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/plugins/bootstrap/js/bootstrap.min.js', array('popper'), '1.0.0', true );
-    wp_enqueue_script( 'wow', get_template_directory_uri() . '/plugins/counterup/wow.min.js', array('jquery'), '1.0.0', true );
-    wp_enqueue_script( 'easing', get_template_directory_uri() . '/plugins/counterup/jquery.easing.1.3.js', array('jquery'), '1.0.0', true );
-    wp_enqueue_script( 'waypoints', get_template_directory_uri() . '/plugins/counterup/jquery.waypoints.js', array('jquery'), '1.0.0', true );
-    wp_enqueue_script( 'counterup', get_template_directory_uri() . '/plugins/counterup/jquery.counterup.min.js', array('jquery'), '1.0.0', true );
-    wp_enqueue_script( 'google-map', get_template_directory_uri() . '/plugins/google-map/gmap3.min.js', array('jquery'), '1.0.0', true );
-    wp_enqueue_script( 'contact', get_template_directory_uri() . '/plugins/jquery/contact.js', array('jquery'), '1.0.0', true );
-    wp_enqueue_script( 'custom', get_template_directory_uri() . '/js/custom.js', array('jquery'), '1.0.0', true );
+    wp_enqueue_script('popper', get_template_directory_uri() . '/plugins/bootstrap/js/popper.min.js', array('jquery'), '1.0.0', true);
+    wp_enqueue_script('bootstrap', get_template_directory_uri() . '/plugins/bootstrap/js/bootstrap.min.js', array('popper'), '1.0.0', true);
+    wp_enqueue_script('wow', get_template_directory_uri() . '/plugins/counterup/wow.min.js', array('jquery'), '1.0.0', true);
+    wp_enqueue_script('easing', get_template_directory_uri() . '/plugins/counterup/jquery.easing.1.3.js', array('jquery'), '1.0.0', true);
+    wp_enqueue_script('waypoints', get_template_directory_uri() . '/plugins/counterup/jquery.waypoints.js', array('jquery'), '1.0.0', true);
+    wp_enqueue_script('counterup', get_template_directory_uri() . '/plugins/counterup/jquery.counterup.min.js', array('jquery'), '1.0.0', true);
+    wp_enqueue_script('google-map', get_template_directory_uri() . '/plugins/google-map/gmap3.min.js', array('jquery'), '1.0.0', true);
+    wp_enqueue_script('contact', get_template_directory_uri() . '/plugins/jquery/contact.js', array('jquery'), '1.0.0', true);
+    wp_enqueue_script('custom', get_template_directory_uri() . '/js/custom.js', array('jquery'), '1.0.0', true);
 }
